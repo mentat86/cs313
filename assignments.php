49d71f67cad4d0218271c83691aa895d1ad88655
Michael Gribble <YamiArchangel@gmail.com>
2016-05-05 21:00:13 -0600

    Adding actual homepage, with assets

---
+++ assignments.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>
+			CS 313 - Assignments Page, Michael Gribble
+		</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="homestyles.css" />
+	</head>
+	<body>
+		<table class="dye">
+			<tr><td><h2>Content pending...</h2></td></tr>
+			<tr><td><a href="homepage.html">Back to Home</a></td></tr>
+		</table>
+	</body>
+</html>
